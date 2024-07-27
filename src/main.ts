f2607b38bfec4659dffd24cf9ff27b69342f13ec
Asterios Patsikas <sergiodk5@gmail.com>
2024-07-27 18:45:01 +0300

    feat: tailwindcss aos

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import AOS from 'aos'
+import './assets/main.css'
+import 'aos/dist/aos.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 
 app.mount('#app')
+
+AOS.init()

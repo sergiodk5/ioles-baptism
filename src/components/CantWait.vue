e9fc3687cb26e2823e647930c35ea0148d59f90a
Asterios Patsikas <sergiodk5@gmail.com>
2024-09-21 21:45:47 +0300

    fix: typo on footer

--- src/components/CantWait.vue
+++ src/components/CantWait.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="bg-white py-10 px-4 text-center">
     <h2 class="mb-4 text-4xl font-bold text-center text-card-pink">
-      We can't wait to share this spacial day with you!<br />
+      We can't wait to share this special day with you!<br />
       🎉🎉💖💖🥳🥳
     </h2>
   </section>
